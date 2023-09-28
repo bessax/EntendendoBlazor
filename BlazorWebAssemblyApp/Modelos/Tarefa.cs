d0a0a61c96feecdb1f100d810ed3a1bf9cb25340
bessax <andrebessax@gmail.com>
2023-09-28 12:17:19 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorWebAssemblyApp/Modelos/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorWebAssemblyApp.Modelos;
+
+public class Tarefa
+{
+    public Guid Id { get; set; }
+    public string? Descricao { get; set; }
+    public bool Concluida { get; set; }
+    public DateTime DataCriacao { set; get; }
+}
